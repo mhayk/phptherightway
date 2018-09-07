c52ea2dabf7904098a287aeb8f3daa5eeaeca425
Mhayk Whandson <hi@mhayk.com>
2018-09-07 17:17:06 +0100

    The Route has been finished.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- app.php
+++ app.php
@@ -2,6 +2,14 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-$router = new Mhayk\Router\Router;
+$path_info = $_SERVER['PATH_INFO'] ?? '/';
+$request_method = $_SERVER['REQUEST_METHOD'] ?? 'GET';
 
+$router = new Mhayk\Router\Router($path_info, $request_method);
 
+$router->get('/hello/{name}', function($params) {
+    return 'My name is ' . $params[0];
+});
+
+$result = $router->run();
+var_dump($result['callback']($result['params']));
